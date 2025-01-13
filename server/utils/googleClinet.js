a7d317d0b367a8e54fd78e0eaaa6b4e592c77fac
ibtisam afzal <ibtisamwarraich101@gmail.com>
2025-01-13 21:20:34 +0500

    secure commit

--- server/utils/googleClinet.js
+++ server/utils/googleClinet.js
@@ -1,8 +1,9 @@
-
+import dotenv from "dotenv"
+dotenv.config({ path: './.env' });
 import { google } from 'googleapis';
 
-const GOOGLE_CLIENT_ID ="86406764343-g6hbgcgne1f6u7nbna9fpctpq5hv9s1d.apps.googleusercontent.com" //process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = "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" //process.env.GOOGLE_CLIENT_SECRET; 
+const GOOGLE_CLIENT_ID =process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET =process.env.GOOGLE_CLIENT_SECRET; 
 
 export const oauth2Client = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
